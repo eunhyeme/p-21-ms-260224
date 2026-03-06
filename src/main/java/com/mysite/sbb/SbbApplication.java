6db0ca24ef872ce31173d35036d7f2ab1e9bd2d4
eunhy <gp79810@gmail.com>
2026-03-06 20:08:59 +0900

    # 1-03: 스프링부트 맛보기

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbbApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SbbApplication.class, args);
+        System.out.println("hello world");
+
     }
 
 }
