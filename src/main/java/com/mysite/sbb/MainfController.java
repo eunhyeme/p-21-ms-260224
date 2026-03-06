f083dc265982570ec97e7f1111712a2946cab500
eunhy <gp79810@gmail.com>
2026-03-06 20:45:12 +0900

    # 2-02: 컨트롤러,URL,@ResponseBody

---
+++ src/main/java/com/mysite/sbb/MainfController.java
@@ -0,0 +1,14 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainfController {
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index(){
+        return "안녕하세요 환영합니다";
+    }
+}
